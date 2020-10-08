3975b1de58be34ccbb4f708b62942dba920c1c0d
Charles Rodriguez <charliefrodriguez@gmail.com>
2020-10-08 14:46:01 -0400

    The graph connect Gameboard functionality has been implemented

--- GameBoard.py
+++ GameBoard.py
@@ -29,9 +29,12 @@ class LinkedList:
         self.head = GraphNode(terrain, path_diff, name)
 
     def insert(self, terrain, path_diff, name):
-        """This method will insert nodes into our linked list """
+        """This method will insert nodes into our linked list, without allowing  duplicate keys"""
         ptr = self.head
         while ptr.after is not None:
+            if ptr is not None and name is ptr.name:
+                print("No duplicate key!")
+                return
             ptr = ptr.after
 
         if ptr.after is None:
@@ -47,17 +50,75 @@ class LinkedList:
 class AdjList:
     """Here we are going to build the adjaceny linked list """
     def __init__(self, x_dim, y_dim):
-        prod = x_dim * y_dim 
-        self.prod = prod
-        self.lst = [0] * (prod)
-        for i in range(prod):
+        dim = x_dim * y_dim
+        self.dim = dim
+        self.lst = [0] * (dim)
+        for i in range(dim):
             self.lst[i] = LinkedList(0, 1, i)
 
     def print_adjl(self):
         """prints out each set of nodes in the graph along with there connections"""
-        for i in range(self.prod):
+        for i in range(self.dim):
             self.lst[i].print_list()
+    def adj_ins(self, list_index, terrain, path_diff, name):
+        """This is insert method for our adjacency linked list """
+        self.lst[list_index].insert(terrain, path_diff, name)
 
+    def screen(self, array):
+        """This method will screen out invalid coordinates"""
+        good_val = [0] * 0
+        for x in array:
+            if x[0] >= 0 and x[1] >= 0 and x[1] < 160 and x[0] < 120:
+                good_val.append(x)
+        return good_val
+
+    def position(self, value):
+        """computes the index of value in the matrix interpreation of the array"""
+        flag = False
+        for i in range(self.dim):
+            if (160 *i <= value and value <= 160*i + 159):
+                indx = i
+                flag = True
+                break
+        if flag is False:
+            return [-1, -1]
+        else:
+            j = -indx*160 +  value
+            if j >= 0:
+                return [indx, j]
+            return [-1, -1]
+
+    def inv_position(self, i, j):
+        """Converts position back to an array value"""
+        return j + i * 160
+
+    def connect(self):
+        """This method links the graph together in the gameboard config """
+        for i in range(self.dim):
+            pos = self.position(i)
+            n_c = [[pos[0]-1, pos[1]], [pos[0]+1, pos[1]], [pos[0], pos[1]+1], [pos[0],\
+            pos[1]-1], [pos[0]-1, pos[1]+1], [pos[0]+1, pos[1]-1], [pos[0]-1, pos[1]-1]\
+            , [pos[0]+1, pos[1]+1]]
+            neighboors = [self.inv_position(x[0], x[1]) for x in self.screen(n_c)]
+            # neighboors = [self.inv_position(x[0], x[1]) for x in n_c]
+            if i is 159: print(n_c)
+            for x in neighboors:
+                if x < 0:
+                    continue
+                self.adj_ins(i, 0, 1, x)
 #R = LinkedList(0, 1, 1)
-LIST = AdjList(3, 2)
-LIST.print_adjl()
+LIST = AdjList(160, 120)
+#LIST.adj_ins(4, 2, 2, 6)
+LIST.connect()
+#print(LIST.position(159))
+#print(LIST.position(160))
+#LIST.lst[0].print_list()
+#print(" ")
+LIST.lst[0].after.terrain = 1
+print(LIST.lst[0].after.terrain)
+LIST.lst[0].print_list()
+#print(" ")
+#LIST.lst[160].print_list()
+#print(" ")
+#LIST.lst[19199].print_list()
+#print(LIST.screen([[-1, 159], [0, 160], [0, 158], [-1, 160], [-1, 158]]))
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 #Path Finder 
 
 I am building a game board for an agent that in a game world. This game board will have dimensions 160x120 and the agent will traverse the board using 
-the A star search algorithm along with two other variations. This is a collaborative effort between me and my friend Padmank.
+the A star search algorithm along with two other variations. This is a collaborative effort between me and my friend Padmank. 
+
+Update: 
+This repository cotains the map that the a star search algo variants will traverse throug
