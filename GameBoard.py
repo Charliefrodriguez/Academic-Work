d9982e9ba6e611ddb3dab6ea24a966ce3f3ab098
Charles Rodriguez <charliefrodriguez@gmail.com>
2020-10-14 00:10:14 -0400

    Put in highways and highways intializer

--- GameBoard.py
+++ GameBoard.py
@@ -3,6 +3,8 @@ import random as rnd
 import math as m
 #import numpy as np
 #We are going to experiment a bit with classes and nodes
+# pylint: disable=R0201
+# pylint: disable=R1705
 class GraphNode:
     """This is my first experiment with a class"""
     def __init__(self, terrain, path_diff, name):
@@ -39,9 +41,12 @@ class LinkedList:
 
     def insert(self, terrain, path_diff, name):
         """This method will insert nodes into our linked list, without allowing  duplicate keys"""
-        ptr = self.head
+        ptr = self.head 
+        if ptr.name == name: 
+           # print("no duplicate keys")
+            return 
         while ptr.after is not None:
-            if ptr is not None and name is ptr.name:
+            if ptr != None and name == ptr.name:
                 print("No duplicate key!")
                 return
             ptr = ptr.after
@@ -69,13 +74,15 @@ class LinkedList:
         else:
             print("not in list")
 
-    def print_list(self):
+    def print_list(self,):
         """ Prints out the members of the linked list """
         ptr = self.head
+        count = 0
         while ptr is not None:
+            count+=1
             print("name " + str(ptr.name) + " " + "terrain " + str(ptr.terrain))
             ptr = ptr.after
-
+        print("len of list "+str(count))
 class AdjList:
     """Here we are going to build the adjaceny linked list """
     def __init__(self, x_dim, y_dim):
@@ -84,6 +91,9 @@ class AdjList:
         self.lst = [0] * (dim)
         for i in range(dim):
             self.lst[i] = LinkedList(1, 1, i)
+        self.connect()
+        for i in range(8):
+            self.set_subgrid()
 
     def print_adjl(self):
         """prints out each set of nodes in the graph along with there connections"""
@@ -131,6 +141,10 @@ class AdjList:
 
     def inv_position(self, i, j):
         """Converts position back to an array value"""
+        if i >= 120 or i < 0:
+            return -1
+        if j > 160 or j < 0:
+            return -1
         return j + i * 160
 
     def connect(self):
@@ -147,18 +161,19 @@ class AdjList:
             for x in neighboors:
                 if x < 0:
                     continue
-                self.adj_ins(i, 0, 1, x)
+                self.adj_ins(i, 1, 1, x)
 
     def set_subgrid(self):
         """sets up the 31x31 subgrid"""
-        i = self.random_coor()[0] 
+        i = self.random_coor()[0]
         j = self.random_coor()[1]
+        #print("["+ str(i) +","+ str(j) +"]")
         c_one = 0
         while c_one < 31:
             c_two = 0
             while c_two < 31:
                 out = self.inv_position(i - 15 + c_one, j - 15 + c_two)
-                print(out)
+                #print(out)
 #                print(str(i-15+c_one) +" "+ str(j-15 +c_two))
                 ter_list = [1, 2]
                 ter_weight = [.50, .50]
@@ -169,22 +184,218 @@ class AdjList:
 
     def random_coor(self):
         """generates random coordinates for 31x31 subgrid"""
-        i = rnd.randint(31, 89)
-        j = rnd.randint(31, 129)
+        i = rnd.randint(30, 88)
+        j = rnd.randint(30, 128)
         return [i, j]
 
+    def highway_start(self):
+        """This function will select the start point of the highway"""
+        i = rnd.randint(0, 120)
+        if i == 0 or i == 120:
+            j = rnd.randint(1, 118) #we don't want the map corners
+        else:
+            j = rnd.choice([0, 119])
+        return [i, j]
+
+    def highway_mov(self, prev_dir):
+        """This will return the direction which the highway will go in """
+
+        set_dir = ['s', 'p']
+        distro = [.60, .20]
+        direction = rnd.choices(set_dir, distro)
+        if direction == 's':
+            if prev_dir == 'd':
+                return 'd'
+            elif prev_dir == 'u':
+                return 'u'
+            elif prev_dir == 'l':
+                return 'l'
+            else:
+                return 'r'
+        else:
+            if prev_dir == 'd':
+                return 'r'
+            elif prev_dir == 'u':
+                return 'l'
+            elif prev_dir == 'l':
+                return 'u'
+            else:
+                return 'd'
+
+
+
+    def path_node_check(self, arr_of_paths, name):
+        """Checks that all nodes inserted into the list don't collide with existing paths """
+        if name == -1: # check for invalid coordinates
+            return 'c'
+
+        if len(arr_of_paths) == 0:
+            return 'o'
+
+        for path in arr_of_paths:
+            ptr = path.head
+            flag = 'o'
+            while ptr != None:
+                if ptr.name == name:
+                    flag = 'c'
+                    break
+                ptr = ptr.after
+
+            if flag == 'c':
+                return 'c'
+            else:
+                return 'o'
+
+
+    def is_boundary(self, i, j):
+        """checks if index value is at the boundary of map """
+        if i == 0 or i == 119:
+            return 0
+        elif j == 0 or j == 160:
+            return 0
+        else:
+            return 1
+
+
+    def highway_set(self):
+        """This function will set up the 4 highways on the map """
+        arr_paths = [None]*0
+       # abort =0
+        while len(arr_paths) < 4:
+        #    abort += 1
+            coor = self.highway_start()
+            path_len = 0
+            if coor[0] == 0:
+                direction = 'd'
+            elif coor[0] == 119:
+                direction = 'u'
+            elif coor[1] == 0:
+                direction = 'r'
+            else:
+                direction = 'l'
+           # print("["+ str(coor[0])+ "," + str(coor[1]) + "]")
+            position = self.inv_position(coor[0], coor[1])
+
+            path = LinkedList(self.lst[position].head.terrain, self.lst[position].head.path_diff,
+                        self.lst[position].head.name)
+         #this is to do the initial iteration at the boundary
+            cur_pos = [coor[0], coor[1]]
+         #   safety =0
+            flag = 0
+            print(" ")
+            path.print_list()
+            print(direction + "\n")
+
+           # while path_len < 100:
+            while self.is_boundary(cur_pos[0],cur_pos[1]):
+          #      safety = safety +1 
+ #               print(" ")
+#                path.print_list()
+           #     if safety == 7:
+            #        break
+                if direction == 'd':
+                    for i in range(20):
+                        temp_pos = self.inv_position(cur_pos[0]+i, cur_pos[1])
+  #                      print("[" +str(cur_pos[0]+i)+ "," +str(cur_pos[1]) + "]")
+                        rating = self.path_node_check(arr_paths, temp_pos)
+                        if rating == 'c':
+                            flag = 1
+                            break
+                        path.insert(self.lst[temp_pos].head.terrain, self.lst[temp_pos].head.path_diff
+                                    , self.lst[temp_pos].head.name)
+                    if flag:
+                        break
+                    cur_pos = [cur_pos[0]+19, cur_pos[1]]
+
+                elif direction == 'u':
+                    for i in range(20):
+                        temp_pos = self.inv_position(cur_pos[0]-i,  cur_pos[1])
+   #                     print("[" +str(cur_pos[0]-i)+ ","+ str(cur_pos[1]) + "]")
+                        rating = self.path_node_check(arr_paths, temp_pos)
+                        if rating == 'c':
+                            flag = 1
+                            break
+                        path.insert(self.lst[temp_pos].head.terrain, self.lst[temp_pos].head.path_diff
+                                    , self.lst[temp_pos].head.name)
+                    if flag:
+                        break
+                    cur_pos = [cur_pos[0]-19, cur_pos[1]]
+
+                elif direction == 'l':
+                    for i in range(20):
+                        temp_pos = self.inv_position(cur_pos[0], cur_pos[1]-i)
+    #                    print("[" +str(cur_pos[0])+ ","+ str(cur_pos[1]-i) + "]")
+                        rating = self.path_node_check(arr_paths, temp_pos)
+                        if rating == 'c':
+                            flag = 1
+                            break
+                        path.insert(self.lst[temp_pos].head.terrain, self.lst[temp_pos].head.path_diff
+                                    , self.lst[temp_pos].head.name)
+                    if flag:
+                        break
+                    cur_pos = [cur_pos[0], cur_pos[1]-19]
+
+                else:
+                    for i in range(20):
+                        temp_pos = self.inv_position(cur_pos[0], cur_pos[1]+i)
+     #                   print("[" +str(cur_pos[0])+ "," +str(cur_pos[1]+i) + "]")
+                        rating = self.path_node_check(arr_paths, temp_pos)
+                        if rating == 'c':
+                            flag = 1
+                            break
+                        path.insert(self.lst[temp_pos].head.terrain, self.lst[temp_pos].head.path_diff
+                                    , self.lst[temp_pos].head.name)
+                    if flag:
+                        break
+                    cur_pos = [cur_pos[0], cur_pos[1]+19]
+
+                direction = self.highway_mov(direction)
+                path_len += 20
+                print(path_len)
+     #       if abort == 5:
+     #           break
+            if flag and path_len > 100:
+                arr_paths.append(path)
+            else:
+                continue
+
+        return arr_paths
+
+    def initialize_h(self):
+        """Intialize the random highways"""
+        out = self.highway_set()
+        for path in out:
+            ptr = path.head
+            while ptr != None:
+                if self.lst[ptr.name].head.name == 1:
+                    self.set_all(ptr.name, 'a')
+                else:
+                    self.set_all(ptr.name, 'b')
+                ptr = ptr.after
+
+
+
 #R = LinkedList(0, 1, 1)
+#R.insert(0,1,1)
+#R.print_list()
 LIST = AdjList(160, 120)
 #LIST.adj_ins(4, 2, 2, 6)
-LIST.connect()
+OUT = LIST.highway_set()
+print(len(OUT))
+OUT[0].print_list()
+OUT[1].print_list()
+OUT[2].print_list()
+OUT[3].print_list()
+LIST.initialize_h()
+LIST.print_adjl()
+#print(OUT[0].head.after.name)
+#print(OUT[2].head.name)
+#print(OUT[3].head.name)
 #print(LIST.position(159))
 #print(LIST.position(160))
 #LIST.set_subgrid(31, 31)
 #LIST.lst[4991].print_list()
-LIST.print_adjl()
 #LIST.lst[4992].print_list()
-#LL = LinkedList(1, 1, 0)
-#LL.insert(1, 1, 1)
-#LIST.print_adjl()
+#LIST.set_subgrid()
 #print(LL.head.after)
 #print(LIST.screen([[-1, 159], [0, 160], [0, 158], [-1, 160], [-1, 158]]))
